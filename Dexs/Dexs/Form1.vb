8e788cdf37a86518b863ae8d7e73380f2fc4572f
Georgia <gbaff357@learner.dvusd.org>
2024-01-16 13:58:04 -0700

    Added all fields

--- Dexs/Dexs/Form1.vb
+++ Dexs/Dexs/Form1.vb
@@ -40,9 +40,22 @@ Public Class Form1
         If IO.File.Exists("data.txt") Then
             Dim inFile As New IO.StreamReader("data.txt")
             records(0) = inFile.ReadLine
-            records(1) = inFile.ReadLine
             inFile.Close()
+            showrecord(0)
         End If
 
+    End Sub
+    Public Sub showrecord(index As Integer)
+        Dim fields() As String
+        fields = records(index).Split("|")
+        field1.Text = fields(0)
+        field2.Text = fields(1)
+        field3.Text = fields(2)
+        field4.Text = fields(3)
+        field5.Text = fields(4)
+        If File.Exists(fields(5)) Then
+            PictureBox1.Load(fields(5))
+
+        End If
     End Sub
 End Class
