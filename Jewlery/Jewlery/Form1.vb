475e55b00f8dff24ac23e8818aacd6d7ec3846a8
Georgia <gbaff357@learner.dvusd.org>
2024-03-27 13:45:52 -0700

    update2

--- Jewlery/Jewlery/Form1.vb
+++ Jewlery/Jewlery/Form1.vb
@@ -10,6 +10,13 @@ Public Class Form1
 
         Me.Show()
     End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+        Dim f2 As New Form2
+        f2.ShowDialog()
+        Label2.Text = f2.2Jewlerycount.Value
+        Me.Show()
+    End Sub
 
     Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
 
