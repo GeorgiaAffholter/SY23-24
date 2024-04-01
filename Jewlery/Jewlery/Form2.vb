f19c9199bb1fc9aee763ee2cfc407ab0166aab62
Georgia <gbaff357@learner.dvusd.org>
2024-04-01 13:26:42 -0700

    Auto stash before merge of "main" and "origin/main"

--- Jewlery/Jewlery/Form2.vb
+++ Jewlery/Jewlery/Form2.vb
@@ -1,4 +1,4 @@
-﻿Public Class Form2
+Public Class Form2
     Dim amount As Integer
     ' Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
     ' amount = amount + 18
